9342e952af2922c65e50a40c5178febf95ec299f
Anirudh Salaria <salariaanirudh@gmail.com>
2023-08-01 21:28:58 +0530

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -9,7 +9,7 @@ const Works = () => {
             <div className='md:flex sm:inline-block border md:p-6 md:py-16 sm:p-0 '>
                 <div className='border md:w-1/2 sm:w-full px-8'>
                     <div className='stylishLine'></div>
-                    <div className='border mt-8 md:w-[85%] sm:w-full'>
+                    <div className='border mt-8 md:w-[95%] sm:w-full'>
                         <div className='font-semibold text-2xl text-lightblue'>
                             Create your free candidate profile
                         </div>
@@ -37,17 +37,63 @@ const Works = () => {
 
                 <div className='border md:w-1/2 sm:w-full px-8'>
                     <div className='stylishLine'></div>
-                    <div className='border mt-8 md:w-[85%] sm:w-full'>
+                    <div className='border mt-8 md:w-[95%] sm:w-full'>
                         <div className='font-semibold text-2xl text-lightblue'>
                             Sit back while companies compete to hire you
                         </div>
                         <div className='py-8 text-[18px] text-jacarta-600'>
-                        Don’t waste your time reaching out to companies or dealing with recruiters.
+                            Don’t waste your time reaching out to companies or dealing with recruiters.
                         </div>
                         <div className=' italic text-jacarta-600'>
                             <div>💨 We fast-track your profile to hiring managers.</div>
                             <div>🙆‍♂️ Get multiple interview requests with upfront salary offers within days.</div>
                         </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* -------------------------------------------------------------------------------------------------------------------- */}
+            <div className='md:flex sm:inline-block border md:p-6 md:py-16 sm:p-0 '>
+                <div className='border md:w-1/2 sm:w-full px-8'>
+                    <div className='stylishLine'></div>
+                    <div className='border mt-8 md:w-[95%] sm:w-full'>
+                        <div className='font-semibold text-2xl text-lightblue'>
+                            Salary & Role transparency
+                        </div>
+                        <div className='py-8 text-[18px] text-jacarta-600'>
+                            Interview requests from companies mention salary and role details upfront - before you decide to accept or decline them.
+                        </div>
+                        {/* <div className=' italic text-jacarta-600'>
+                            <div>💨 We fast-track your profile to hiring managers.</div>
+                            <div>🙆‍♂️ Get multiple interview requests with upfront salary offers within days.</div>
+                        </div> */}
+                    </div>
+                </div>
+
+                <div className='md:w-1/2 sm:w-full border'>
+                    <img src="https://d383au3bye3rv1.cloudfront.net/static/images/optimized/C%20-%20Transparency.png" alt="img" />
+                </div>
+            </div>
+
+            {/* -------------------------------------------------------------------------------------------------------------------- */}
+            <div className='md:flex sm:inline-block border md:p-6 md:py-16 sm:p-0 '>
+                <div className='md:w-1/2 sm:w-full border'>
+                    <img src="https://d383au3bye3rv1.cloudfront.net/static/images/optimized/C%20-%20Dedicated%20Talent%20Advocate.png" alt="img" />
+                </div>
+
+                <div className='border md:w-1/2 sm:w-full px-8'>
+                    <div className='stylishLine'></div>
+                    <div className='border mt-8 md:w-[95%] sm:w-full'>
+                        <div className='font-semibold text-2xl text-lightblue'>
+                            A dedicated talent advocate
+                        </div>
+                        <div className='py-8 text-[18px] text-jacarta-600'>
+                            Your Talent Advocate will guide you through the entire process. From polishing your profile, providing interview tips to helping negotiate offers!
+                        </div>
+                        {/* <div className=' italic text-jacarta-600'>
+                            <div>💨 We fast-track your profile to hiring managers.</div>
+                            <div>🙆‍♂️ Get multiple interview requests with upfront salary offers within days.</div>
+                        </div> */}
                     </div>
                 </div>
             </div>
