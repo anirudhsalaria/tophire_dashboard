3f00c54e2a3296815484dadeb113de43ba7c2f9b
Anirudh Salaria <salariaanirudh@gmail.com>
2023-08-01 21:31:35 +0530

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -99,6 +99,29 @@ const Works = () => {
             </div>
 
             {/* -------------------------------------------------------------------------------------------------------------------- */}
+            <div className='md:flex sm:inline-block border md:p-6 md:py-16 sm:p-0 '>
+                <div className='border md:w-1/2 sm:w-full px-8'>
+                    <div className='stylishLine'></div>
+                    <div className='border mt-8 md:w-[95%] sm:w-full'>
+                        <div className='font-semibold text-2xl text-lightblue'>
+                        Current and past employers can’t see your profile
+                        </div>
+                        <div className='py-8 text-[18px] text-jacarta-600'>
+                        By default, we hide your profile from any employers that you list as your current and past employer.
+                        </div>
+                        <div className=' italic text-jacarta-600'>
+                            <div>🕵 Privacy Assured.</div>
+                            {/* <div>🙆‍♂️ Get multiple interview requests with upfront salary offers within days.</div> */}
+                        </div>
+                    </div>
+                </div>
+
+                <div className='md:w-1/2 sm:w-full border'>
+                    <img src="https://d383au3bye3rv1.cloudfront.net/static/images/optimized/C%20-%20Privacy.png" alt="img" />
+                </div>
+            </div>
+
+            {/* -------------------------------------------------------------------------------------------------------------------- */}
 
         </div>
     )
