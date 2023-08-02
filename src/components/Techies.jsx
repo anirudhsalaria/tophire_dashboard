6c405ae4499734abdeb38eb241fe58ffdd7b1bf9
Anirudh Salaria <salariaanirudh@gmail.com>
2023-08-02 14:54:15 +0530

    Techies compo

--- src/components/Techies.jsx
+++ src/components/Techies.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
+
 const Techies = () => {
   return (
     <div className='w-full techiesimg border'>
-      <div className='container text-jacarta-900'>
-        Hello
+      <div className='container py-8'>
+        <div className='border text-center'>
+          <div className='font-bold text-4xl border'>Techies 💘 TopHire</div>
+          <div className='border mt-6 text-lg text-jacarta-700'>A better love story than twilight. Check the video below to find out why.</div>
+          <div className='border mt-14'>
+           <img className='m-auto' src="https://cdn.jwplayer.com/v2/media/CAQeMfpE/poster.jpg?width=640" alt="img" height={303} width={540}/>
+          </div>
+        </div>
       </div>
     </div>
   )
