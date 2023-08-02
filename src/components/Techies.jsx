5a214ec48d944b8da42ce41f482c15529aef5f70
Anirudh Salaria <salariaanirudh@gmail.com>
2023-08-02 15:10:21 +0530

    additions

--- src/components/Techies.jsx
+++ src/components/Techies.jsx
@@ -9,8 +9,75 @@ const Techies = () => {
           <div className='font-bold text-4xl border'>Techies 💘 TopHire</div>
           <div className='border mt-6 text-lg text-jacarta-700'>A better love story than twilight. Check the video below to find out why.</div>
           <div className='border mt-14'>
-           <img className='m-auto' src="https://cdn.jwplayer.com/v2/media/CAQeMfpE/poster.jpg?width=640" alt="img" height={303} width={540}/>
+            <img className='m-auto' src="https://cdn.jwplayer.com/v2/media/CAQeMfpE/poster.jpg?width=640" alt="img" height={303} width={540} />
           </div>
+        </div>
+
+
+        <div className='container border mt-16 grid grid-cols-3 p-3'>
+
+          <div className='border'>
+            <div className='border'>
+              <img src="https://d383au3bye3rv1.cloudfront.net/static/images/optimized/cred.png?123" alt="img" height={40} width={118} />
+            </div>
+
+            <div className='flex border mt-5 gap-3'>
+              <div>
+                <img src="https://d383au3bye3rv1.cloudfront.net/static/images/optimized/yadhu_manoharan.jpg?123" alt="img" height={50} width={50} className='rounded-full' />
+              </div>
+              <div className='border'>
+                <div className='font-semibold text-[16px]'>Yadhu Manoharan</div>
+                <div className='text-[14px] font-light'>Sr. iOS Engineer</div>
+              </div>
+            </div>
+
+            <div className='border mt-5 text-jacarta-600'>
+              TopHire was able to make the process really easy. They were able to schedule interviews with the best companies in the industry and drove the entire process smoothly.
+            </div>
+          </div>
+
+          
+          <div className='border'>
+            <div className='border'>
+              <img src="https://d383au3bye3rv1.cloudfront.net/static/images/optimized/cred.png?123" alt="img" height={40} width={118} />
+            </div>
+
+            <div className='flex border mt-5 gap-3'>
+              <div>
+                <img src="https://d383au3bye3rv1.cloudfront.net/static/images/optimized/yadhu_manoharan.jpg?123" alt="img" height={50} width={50} className='rounded-full' />
+              </div>
+              <div className='border'>
+                <div className='font-semibold text-[16px]'>Yadhu Manoharan</div>
+                <div className='text-[14px] font-light'>Sr. iOS Engineer</div>
+              </div>
+            </div>
+
+            <div className='border mt-5 text-jacarta-600'>
+              TopHire was able to make the process really easy. They were able to schedule interviews with the best companies in the industry and drove the entire process smoothly.
+            </div>
+          </div>
+
+
+          <div className='border'>
+            <div className='border'>
+              <img src="https://d383au3bye3rv1.cloudfront.net/static/images/optimized/cred.png?123" alt="img" height={40} width={118} />
+            </div>
+
+            <div className='flex border mt-5 gap-3'>
+              <div>
+                <img src="https://d383au3bye3rv1.cloudfront.net/static/images/optimized/yadhu_manoharan.jpg?123" alt="img" height={50} width={50} className='rounded-full' />
+              </div>
+              <div className='border'>
+                <div className='font-semibold text-[16px]'>Yadhu Manoharan</div>
+                <div className='text-[14px] font-light'>Sr. iOS Engineer</div>
+              </div>
+            </div>
+
+            <div className='border mt-5 text-jacarta-600'>
+              TopHire was able to make the process really easy. They were able to schedule interviews with the best companies in the industry and drove the entire process smoothly.
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
